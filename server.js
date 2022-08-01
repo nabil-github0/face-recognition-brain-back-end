1aa21070262122cf08ddaf6db2544cbc7fef43f6
nabil-github0 <nabilahmed.2977.2019@gmail.com>
2022-08-01 02:47:41 -0700

    Adding PORT

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.put("/image", (req, res) => {image.handleImage(req, res, knex)});
 
 app.post("/imageurl", (req, res) => {image.handleApiCall(req, res)});
 
-app.listen(3000, () => {
-  console.log("app is runnig on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is runnig on port ${process.env.PORT}`);
 });
