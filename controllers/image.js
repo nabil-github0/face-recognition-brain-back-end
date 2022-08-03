43f8c2335514429f095ade0310b23e61e0932dc2
nabil-github0 <nabilahmed.2977.2019@gmail.com>
2022-08-03 04:12:52 -0700

    Converting REST into grpc

--- controllers/image.js
+++ controllers/image.js
@@ -1,16 +1,50 @@
-const clarifai = require("clarifai");
+const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
 
-const app = new Clarifai.App({
-  apiKey: process.env.API_KEY_CLARIFAI
-});
+const stub = ClarifaiStub.grpc();
 
+const metadata = new grpc.Metadata();
+metadata.set("authorization", `Key ${process.env.API_KEY_CLARIFAI}`);
+
+// const clarifai = require("clarifai");
+// console.log(clarifai);
+
+// const app = new Clarifai.App({
+//   apiKey: process.env.API_KEY_CLARIFAI
+// });
 const handleApiCall = (req, res) => {
-  app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-  .then(data => {
-    res.json(data);
-  })
-  .catch(err => res.status(400).json("Unable to work with api"))
+stub.PostModelOutputs(
+  {
+      // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
+      model_id: FACE_DETECT_MODEL,
+      inputs: [{data: {image: {url: req.body.input}}}]
+  },
+  metadata,
+  (err, response) => {
+      if (err) {
+          console.log("Error: " + err);
+          return;
+      }
+
+      if (response.status.code !== 10000) {
+          console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
+          return;
+      }
+
+      console.log("Predicted concepts, with confidence values:")
+      for (const c of response.outputs[0].data.concepts) {
+          console.log(c.name + ": " + c.value);
+      }
+      res.json(response)
+  }
+)
 }
+
+  // app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+  // .then(data => {
+  //   res.json(data);
+  // })
+  // .catch(err => res.status(400).json("Unable to work with api"))
+
 
 const handleImage =  (req, res, knex) => {
     const { id } = req.body;
